795dca2a63e4bf5537eeb2ffe25a9e02361a7ae8
pnadalini <26207809+pnadalini@users.noreply.github.com>
2025-07-11 06:03:53 -0600

    Add custom favicon and change title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Head from "next/head";
 
 const Home = () => {
-  return <div className="p-5">Home page</div>;
+  return (
+    <div className="p-5">
+      <Head>
+        <title>Dynamic Navbar</title>
+      </Head>
+      Home page
+    </div>
+  );
 };
 
 export default Home;
